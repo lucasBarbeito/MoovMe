1dec8cbe0cb07af02803eded1ac625201ef1b0af
Georges <georges.stefan@ing.austral.edu.ar>
2019-10-03 14:50:05 -0300

    agregue un comentario

--- src/Members/Administrator.java
+++ src/Members/Administrator.java
@@ -2,7 +2,6 @@ package Members;
 
 public class Administrator extends User {
 
-
     public Administrator(String name,String lastName, String userName, String password, String phoneNumber){
         super(name, lastName, userName, password, phoneNumber);
         adminStatus = true;
@@ -10,7 +9,5 @@ public class Administrator extends User {
 
     // public void setAdminStatus(Boolean adminStatus) { this.adminStatus = adminStatus; }
     public Boolean getAdminStatus() { return adminStatus; }
-
-//hhhhhhhhhhhhhhh
 
 }
