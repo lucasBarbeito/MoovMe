e1a9c860eb6fd53aa688bddc676c00db6f2924d1
Georges <georges.stefan@ing.austral.edu.ar>
2019-10-08 12:31:22 -0300

    Elimine la variable userName

--- src/Registration.java
+++ src/Registration.java
@@ -3,7 +3,7 @@ import Members.User;
 
 public class Registration {
 
-    public void registerUser(String firstName, String lastName, String userName, String phoneNumber, UserABM userABM) {
+    public void registerUser(String firstName, String lastName, String phoneNumber, UserABM userABM) {
         User user = new User(firstName, lastName, phoneNumber);
         userABM.add(user);
     }
