1e1f6d17c79604a8b31dcdb43132c0951b40803c
Georges <georges.stefan@ing.austral.edu.ar>
2019-10-08 12:36:56 -0300

    Elimine la variable userName

--- src/Members/User.java
+++ src/Members/User.java
@@ -7,24 +7,18 @@ import java.util.HashMap;
 
 public class User {
 
-    protected String firstName, lastName, userName, phoneNumber;
-    private boolean isBlocked;
+    protected String firstName, lastName, phoneNumber;
     HashMap<Integer, ScorePoint> points = new HashMap<Integer, ScorePoint>();
 
-    public User(String firstName, String lastName, String userName, String phoneNumber){
+    public User(String firstName, String lastName, String phoneNumber){
         this.firstName = firstName;
         this.lastName = lastName;
-        this.userName = userName;
         this.phoneNumber = phoneNumber;
-        isBlocked = false;
     }
 
     public String getFirstName() { return firstName;}
     public String getLastName() {
         return lastName;
-    }
-    public String getUserName() {
-        return userName;
     }
     public String getPhoneNumber() {
         return phoneNumber;
