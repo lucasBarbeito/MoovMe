e8d3e88f5c21766814f8d60d0e48b0029bf9092b
Georges <georges.stefan@ing.austral.edu.ar>
2019-10-08 13:53:50 -0300

    Cree el metodo printAllUsers en UserABM y la clase MoovMeTest con el main

--- src/Members/Administrator.java
+++ src/Members/Administrator.java
@@ -5,5 +5,4 @@ public class Administrator extends User {
     public Administrator(String firstName, String lastName, String phoneNumber){
         super(firstName, lastName, phoneNumber);
     }
-
 }
