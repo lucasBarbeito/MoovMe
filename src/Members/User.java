1293d5638297d72895ec7fc2988426396c702b10
maiacamarero <maia.camarero@ing.austral.edu.ar>
2019-10-08 12:30:38 -0300

    agregue el userId en user y scorePoint

--- src/Members/User.java
+++ src/Members/User.java
@@ -7,18 +7,24 @@ import java.util.HashMap;
 
 public class User {
 
-    protected String firstName, lastName, phoneNumber;
+    protected String firstName, lastName, userName, phoneNumber;
+    private boolean isBlocked;
     HashMap<Integer, ScorePoint> points = new HashMap<Integer, ScorePoint>();
 
-    public User(String firstName, String lastName, String phoneNumber){
+    public User(String firstName, String lastName, String userName, String phoneNumber){
         this.firstName = firstName;
         this.lastName = lastName;
+        this.userName = userName;
         this.phoneNumber = phoneNumber;
+        isBlocked = false;
     }
 
     public String getFirstName() { return firstName;}
     public String getLastName() {
         return lastName;
+    }
+    public String getUserName() {
+        return userName;
     }
     public String getPhoneNumber() {
         return phoneNumber;
